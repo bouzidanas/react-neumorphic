e19190d3b0b9cb3b6b9a11abb1fb33b66d730090
bouzidanas <anasbouzid@gmail.com>
2025-08-25 19:48:44 -0500

    add demo build

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { resolve } from 'path'
-import { fileURLToPath, URL } from 'url'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    lib: {
-      entry: {
-        index: resolve(fileURLToPath(new URL('.', import.meta.url)), 'src/index.ts'),
-        'toggle/index': resolve(fileURLToPath(new URL('.', import.meta.url)), 'src/components/toggle/index.ts'),
-      },
-      name: 'ReactNeumorphic',
-      formats: ['es', 'cjs'],
-      fileName: (format, entryName) => {
-        const extension = format === 'es' ? 'esm.js' : 'cjs.js'
-        return `${entryName}.${extension}`
-      }
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM'
-        }
-      }
-    }
-  }
-})
+    outDir: 'dist', // Output directory for deployment
+  },
+});
